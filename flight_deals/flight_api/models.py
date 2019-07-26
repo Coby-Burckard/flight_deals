ce1acbb35ff27b11917c6e30b7caa3082471c2a5
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2019-07-26 12:49:40 -0500

    starting api work - might delete for REST api

--- flight_deals/flight_api/models.py
+++ flight_deals/flight_api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Deal(models.Model):
+    date_created = models.DateField()
+    date_removed = models.DateField()
+    active = models.BooleanField(null=False)
+
+    class Meta():
+        ordering = ['-date_created']
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        cities = self.Airport_set.all()
+        if cities in other.Airport_set.all():
+            return True
+        else:
+            return False
+        pass
+
+class Airport(models.Model):
+    deal = models.ForeignKey(Deal, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 10)
