c72b2a322b4d357784e17583632578521488f30c
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2019-07-28 08:16:46 -0500

    creating views outline

--- flight_rest_api/skymiles_tracker/views.py
+++ flight_rest_api/skymiles_tracker/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.csfr import csfr_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from .serializers import DealSerializer, AirportSerializer
+from .models import Deal, Airport
+
+@csfr_exempt
+def get_deals(request):
+    '''
+        GET: returns a list of deals
+        POST: scrapes the delta page for deals and updates the database.
+    '''
+
+def verify_deal(deal):
+    '''
+        verifies the components of the deal, and updates necessary information in the database.
+    '''
+
+def verify_airport(airport):
+    '''
+        verifies that an airport exists and adds one to the database if not.
+    '''
