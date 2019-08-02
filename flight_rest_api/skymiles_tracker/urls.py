a345fa06f19df10dcf355ddb4992a2d6dd584134
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2019-08-02 16:04:37 -0500

    simplified arguments passed to get_history

--- flight_rest_api/skymiles_tracker/urls.py
+++ flight_rest_api/skymiles_tracker/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('skymiles_tracker/deals/', views.get_deals),
-    path('skymiles_tracker/get_history/<str:departure_airport>/<str:arrival_airport>/', views.get_history),
+    path('skymiles_tracker/get_history/<int:deal_id>/', views.get_history),
 ]
--- flight_rest_api/skymiles_tracker/views.py
+++ flight_rest_api/skymiles_tracker/views.py
@@ -8,6 +8,7 @@ from rest_framework.decorators import api_view
 from .serializers import DealSerializer, AirportSerializer
 from .models import Deal, Airport
 
+@csrf_exempt
 @api_view(['GET'])
 def get_deals(request):
     '''
@@ -19,13 +20,15 @@ def get_deals(request):
         serializer = DealSerializer(deals, many=True)
         return Response(serializer.data)
 
+@csrf_exempt
 @api_view(['GET'])
-def get_history(request, departure_airport, arrival_airport):
+def get_history(request, deal_id):
     '''
-        GET: returns a history of a flight between two given airports
+        GET: returns a history of a flight between two given airports. Order by date created.
     '''
 
     if request.method == 'GET':
-        deals = Deal.objects.filter(departure_airport__name=departure_airport, arrival_airport__name=arrival_airport).order_by('-created')
+        deal = Deal.objects.get(pk=deal_id)
+        deals = Deal.objects.filter(departure_airport__name=deal.departure_airport, arrival_airport__name=deal.arrival_airport).order_by('-created')
         serializer = DealSerializer(deals, many=True)
         return Response(serializer.data)
